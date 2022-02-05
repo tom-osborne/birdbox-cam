3ffe7a8dbc05a6703e72ed20d583d4fbccb9c2b7
tom-osborne <tom.j.osborne@gmail.com>
2022-02-05 13:17:59 +0000

    just trying raspivid

--- bboxcam.py
+++ bboxcam.py
@@ -9,7 +9,7 @@ print("Started BirdBox Cam v0.1")
 # raspivid -o - -t 0 -vf -hf -fps 30 -b 6000000 | ffmpeg -re -ar 44100 -ac 2 -acodec pcm_s16le -f s16le -ac 2 -i /dev/zero -f h264 -i - -vcodec copy -acodec aac -ab 128000 -g 50 -strict experimental -f flv rtmp://a.rtmp.youtube.com/live2/7ytq-b3uh-xrr5-0hmu-6x61
 YOUTUBE="rtmp://a.rtmp.youtube.com/live2/" 
 KEY="7ytq-b3uh-xrr5-0hmu-6x61"
-stream_cmd = 'raspivid -o - -t 0 -vf -hf -fps 30 -b 6000000 | ffmpeg -f h264 -r 25 -i - -itsoffset 5.5 -fflags nobuffer -f alsa -ac 1 -i /dev/zero -vcodec copy -acodec aac -ac 1 -ar 8000 -ab 32000 -map 0:0 -map 1:0 -strict experimental -f flv ' + YOUTUBE + KEY 
+stream_cmd = 'raspivid -o - -t 0 -vf -hf -fps 30 -b 6000000 | ffmpeg -re -ar 44100 -ac 2 -acodec pcm_s16le -f s16le -ac 2 -i /dev/zero -f h264 -i - -vcodec copy -acodec aac -ab 128000 -g 50 -strict experimental -f flv rtmp://a.rtmp.youtube.com/live2/7ytq-b3uh-xrr5-0hmu-6x61'
 stream_pipe = subprocess.Popen(stream_cmd, shell=True, stdin=subprocess.PIPE) 
 # # camera = picamera.PiCamera(resolution=(640, 480), framerate=25) 
 # try: 

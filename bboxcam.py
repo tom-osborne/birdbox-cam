71d236603b4d128bd255d78ae38b04eb8146f74d
tom-osborne <tom.j.osborne@gmail.com>
2022-02-05 15:08:22 +0000

    adding text overlay

--- bboxcam.py
+++ bboxcam.py
@@ -4,7 +4,7 @@ import time
 
 print("Started BirdBox Cam v0.1")
 
-raspivid_cmd = 'raspivid -o - -t 0 -a "%d %B %Y %H:%M:%S" -vf -hf -fps 30 -b 6000000'
+raspivid_cmd = 'raspivid -o - -t 0 -a "%d %B %Y %H:%M:%S" -ae 10,0x8080ff,0x00 -vf -hf -fps 30 -b 6000000'
 ffmpeg_cmd = 'ffmpeg -re -ar 44100 -ac 2 -acodec pcm_s16le -f s16le -ac 2 -i /dev/zero -f h264 -i - -vcodec copy -acodec aac -ab 128000 -g 50 -strict experimental -f flv rtmp://a.rtmp.youtube.com/live2/7ytq-b3uh-xrr5-0hmu-6x61'
 stream_cmd = raspivid_cmd + ' | ' + ffmpeg_cmd
 

92abcd10bf48c0ee18e93bfa4998286ee1b21ed2
tom-osborne <tom.j.osborne@gmail.com>
2022-02-05 15:22:38 +0000

    troubleshooting overlay

--- bboxcam.py
+++ bboxcam.py
@@ -5,7 +5,7 @@ import time
 print("Started BirdBox Cam v0.1")
 
 
-raspivid_cmd = 'raspivid -o - -t 0 -a "pi2cam %d %B %Y %H:%M:%S" -ae 24,0x00,0x8080ff -vf -hf -fps 30 -b 6000000'
+raspivid_cmd = 'raspivid -o - -t 0 -a 1024 -a "%Y-%m-%d %X" -vf -hf -fps 30 -b 6000000'
 ffmpeg_cmd = 'ffmpeg -re -ar 44100 -ac 2 -acodec pcm_s16le -f s16le -ac 2 -i /dev/zero -f h264 -i - -vcodec copy -use_wallclock_as_timestamps 1 -acodec aac -ab 128000 -g 50 -strict experimental -f flv rtmp://a.rtmp.youtube.com/live2/7ytq-b3uh-xrr5-0hmu-6x61'
 stream_cmd = raspivid_cmd + ' | ' + ffmpeg_cmd
 

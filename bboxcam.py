029daa0581fb23ccd96cd207bbe995974361fae6
tom-osborne <tom.j.osborne@gmail.com>
2022-02-05 11:35:11 +0000

    corrected stream key

--- bboxcam.py
+++ bboxcam.py
@@ -6,7 +6,7 @@ import time
 print("Started BirdBox Cam v0.1")
 
 YOUTUBE="rtmp://a.rtmp.youtube.com/live2/" 
-KEY=7ytq-b3uh-xrr5-0hmu-6x61 
+KEY="7ytq-b3uh-xrr5-0hmu-6x61"
 stream_cmd = 'ffmpeg -f h264 -r 25 -i - -itsoffset 5.5 -fflags nobuffer -f alsa -ac 1 -i hw:1,0 -vcodec copy -acodec aac -ac 1 -ar 8000 -ab 32k -map 0:0 -map 1:0 -strict experimental -f flv ' + YOUTUBE + KEY 
 stream_pipe = subprocess.Popen(stream_cmd, shell=True, stdin=subprocess.PIPE) 
 camera = picamera.PiCamera(resolution=(640, 480), framerate=25) 

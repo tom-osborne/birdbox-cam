a91968504846d48a5f71ffa8769350cf779ee45b
tom-osborne <tom.j.osborne@gmail.com>
2022-02-06 12:52:07 +0000

    re-packaged

---
+++ launcher.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+# launcher.sh
+# navigate to home directory, then to this directory, then execute python script
+sleep 5
+
+# raspivid -o - -t 0 -vf -fps 30 -b 6000000 | ffmpeg -re -ar 44100 -ac 2 -acodec pcm
+cd home/pi/birdbox-cam/
+sudo python main.py
+cd /
+
+

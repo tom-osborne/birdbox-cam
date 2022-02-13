ae0a4519c8a698e8f11c1e87f06a96f327ec0391
tom-osborne <tom.j.osborne@gmail.com>
2022-02-13 13:21:32 +0000

    switched account

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3 
 import subprocess
+import time
 
 print("Started BirdBox Cam v0.1")
 
 raspivid_cmd = 'raspivid -o - -t 0 -a 1036 -a "%Y-%m-%d %H:%M:%S" -vf -hf -fps 30 -b 10000000'
-ffmpeg_cmd = "ffmpeg -re -ar 44100 -ac 2 -acodec pcm_s16le -f s16le -ac 2 -i /dev/zero -f h264 -i - -vcodec copy -acodec aac -ab 128k -g 50 -strict experimental -f flv rtmp://a.rtmp.youtube.com/live2/7ytq-b3uh-xrr5-0hmu-6x61"
+ffmpeg_cmd = "ffmpeg -re -ar 44100 -ac 2 -acodec pcm_s16le -f s16le -ac 2 -i /dev/zero -f h264 -i - -vcodec copy -acodec aac -ab 128k -g 50 -strict experimental -f flv rtmp://a.rtmp.youtube.com/live2/wj3x-sh5e-pxh9-rmd2-cew4"
 stream_cmd = raspivid_cmd + ' | ' + ffmpeg_cmd
 
 stream_pipe = subprocess.Popen(stream_cmd, shell=True, stdin=subprocess.PIPE) 

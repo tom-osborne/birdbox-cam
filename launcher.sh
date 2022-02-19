a46e44e163f55982ca85aea73bffdbba9a7ee374
tom-osborne <tom.j.osborne@gmail.com>
2022-02-19 18:35:25 +0000

    reduced bitrate and added vncserver on startup

--- launcher.sh
+++ launcher.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 # launcher.sh
 # navigate to home directory, then to this directory, then execute python script
+sleep 10
+
+vncserver
+
 sleep 5
 
 # raspivid -o - -t 0 -vf -fps 30 -b 6000000 | ffmpeg -re -ar 44100 -ac 2 -acodec pcm
--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import config
 
 print("Started BirdBox Cam v0.1")
 
-raspivid_cmd = 'raspivid -o - -t 0 -a 1036 -a "%Y-%m-%d %H:%M:%S" -vf -hf -fps 30 -b 10000000'
+raspivid_cmd = 'raspivid -o - -t 0 -a 1036 -a "%Y-%m-%d %H:%M:%S" -fps 30 -b 4500000'
 ffmpeg_cmd = f"ffmpeg -re -ar 44100 -ac 2 -acodec pcm_s16le -f s16le -ac 2 -i /dev/zero -f h264 -i - -vcodec copy -acodec aac -ab 128k -g 50 -strict experimental -f flv rtmp://a.rtmp.youtube.com/live2/{config.STREAM_KEY}"
 stream_cmd = raspivid_cmd + ' | ' + ffmpeg_cmd
 
